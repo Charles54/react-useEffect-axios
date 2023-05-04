8f4a0546f07f7ef16c156f8d8d22179e6e411117
Prince Ndukwe <princendu54@gmail.com>
2023-05-04 12:25:41 +0100

    axios added

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import axios from 'axios';
 
 function FetchData() {
     const [records, setRecords] = useState([]);
         
         
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        //fetch('https://jsonplaceholder.typicode.com/users')
+        axios.get('https://jsonplaceholder.typicode.com/users')
         .then((response) => response.json())
         .then((data) => setRecords(data))
         .catch((err) => console.log(err))
